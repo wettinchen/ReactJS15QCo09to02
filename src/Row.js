23ab41170ee82b5b2cf531b5f0ac6d2062ad1bd2
suncrownfisher <suncrownfisher@gmail.com>
2023-04-16 01:28:29 +0800

    first commit

---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Row = ({ item }) => {
+  return (
+    <tr>
+        {Object.entries(item).map(([key, value]) => {
+            return (
+
+            )
+        })}
+    </tr>
+  )
+}
+
+export default Row
